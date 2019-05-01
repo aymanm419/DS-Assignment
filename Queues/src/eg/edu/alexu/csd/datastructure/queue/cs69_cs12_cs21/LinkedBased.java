3fef9ea3eb7325fc6d5ea775aab085b8ece2684d
aymanm419 <40718764+aymanm419@users.noreply.github.com>
2019-05-01 14:05:06 +0200

    Cleaned Files & organized them

---
+++ Queues/src/eg/edu/alexu/csd/datastructure/queue/cs69_cs12_cs21/LinkedBased.java
@@ -0,0 +1,32 @@
+package eg.edu.alexu.csd.datastructure.queue.cs69_cs12_cs21;
+
+public class LinkedBased implements IQueue,ILinkedBased {
+    LinkedList q = new LinkedList();
+
+    public void enqueue(Object c) {
+        q.add(c);
+    }
+
+    public Object dequeue() {
+        if (q.isEmpty() == false) {
+            Object c = q.get(0);
+            q.remove(0);
+            return c;
+        } else {
+            throw new RuntimeException("Queue is Empty");
+        }
+
+    }
+
+    public boolean isEmpty() {
+        if (q.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int size() {
+        return q.getlength();
+    }
+}
