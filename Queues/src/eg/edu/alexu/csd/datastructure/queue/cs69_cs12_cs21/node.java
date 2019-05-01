038a4a529c06a6113d16992a594d728c431f3533
bkhalf <bahaakhalf8@gmail.com>
2019-05-01 13:42:31 +0200

    Added queue implementation in both array & linkedlist based

---
+++ Queues/src/eg/edu/alexu/csd/datastructure/queue/cs69_cs12_cs21/node.java
@@ -0,0 +1,18 @@
+package eg.edu.alexu.csd.datastructure.queue.cs69_cs12_cs21;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Node For linked list
+ * @author 
+ */
+public class node {
+     node  next =null;
+     Object value;
+    
+}
+
