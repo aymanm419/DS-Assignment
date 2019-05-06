958a33088300a059ab4363d6377ea1a5e1bdce26
aymanm419 <40718764+aymanm419@users.noreply.github.com>
2019-05-06 22:47:19 +0200

    Add Basic Folders

---
+++ Maze/src/eg/edu/alexu/csd/datastructure/maze/cs69_cs12_cs21/Main.java
@@ -0,0 +1,8 @@
+package eg.edu.alexu.csd.datastructure.maze.cs69_cs12_cs21;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Maze/src/eg/edu/alexu/csd/datastructure/maze/cs69_cs12_cs21/MazeSolver.java
@@ -0,0 +1,11 @@
+package eg.edu.alexu.csd.datastructure.maze.cs69_cs12_cs21;
+
+public class MazeSolver implements IMazeSolver {
+    public int[][] solveBFS(java.io.File maze) {
+        return null;
+    }
+
+    public int[][] solveDFS(java.io.File maze) {
+        return null;
+    }
+}
